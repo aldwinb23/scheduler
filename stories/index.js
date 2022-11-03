9f41041ed21241b40774a302b0b6216d30ffa851
aldwinb <aldwinb23@gmail.com>
2022-11-02 23:01:05 -0700

    add Error and Status components and story

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
 
 
 storiesOf("Button", module)
@@ -211,4 +213,10 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error 
+      message="Could not delete appointment"
+      onClose={action("onClose")}
+    /> 
+  ))
